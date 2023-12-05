9c62e5cd6dcff5e1acfb175b465131260206641c
Lucas Santana <lucasadd15@gmail.com>
2023-12-05 14:37:54 -0300

    alter cod

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Prompt } from "@/components/card";
 import Modal from "@/components/poupop";
 import Image from "next/image";
 import { useState } from "react";
@@ -33,13 +34,14 @@ export default function Home() {
         </button>
       </div>
 
-      {ispoupop && <Modal />}
+      {ispoupop && <Modal setIspoupop={setIspoupop} />}
       <iframe
         src="https://discord.com/widget?id=1181632791935471636&theme=dark"
         width="350"
         height="500"
         sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
       ></iframe>
+      <Prompt />
     </main>
   );
 }
--- components/poupop.tsx
+++ components/poupop.tsx
@@ -2,8 +2,14 @@
 import { Cadastro } from "@/function/cadastro";
 import { VerificarUserExistente } from "@/function/verificarseexiste";
 import { useState } from "react";
+import { IoIosClose } from "react-icons/io";
 
-export default function Modal() {
+
+interface IUrecev {
+  setIspoupop: any;
+}
+
+export default function Modal({setIspoupop}: IUrecev) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
@@ -17,13 +23,27 @@ export default function Modal() {
     if (!verificar) {
       Cadastro(username, password);
       setSucess("Conta Criada Com Sucesso!!!");
+      setTimeout(()=> {
+        setIspoupop(false)
+      }, 2000)
+
+
     } else {
       setSucess("Essa Conta já existe em Nosso Cadastro!!!");
     }
   }
 
+  function Close() {
+    setIspoupop(false);
+  }
+
   return (
-    <div className="fixed bg-cyan-600 w-[30%] h-auto gap-4 p-10 shadow-xl shadow-cyan-400">
+    <div className="fixed bg-cyan-600 w-[30%] h-auto gap-4 p-10 shadow-xl shadow-cyan-400 rounded-md">
+      <div className="flex justify-end">
+        <button onClick={Close}>
+          <IoIosClose size={34} />
+        </button>
+      </div>
       <h1 className="h-10">Criar Conta</h1>
       <div className="flex flex-col justify-center items-center gap-2 ">
         <input
