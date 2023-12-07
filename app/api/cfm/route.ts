6db6268d7468984243b4934c5e774da895fd1cec
Lucas Santana <lucasadd15@gmail.com>
2023-12-06 23:16:37 -0300

    alter codigo

--- app/api/cfm/route.ts
+++ app/api/cfm/route.ts
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 import { Consultar } from "@/function/consultarcodigo";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -6,9 +6,16 @@ export async function GET(request: NextRequest) {
 	const { searchParams } = new URL(request.url);
 	const id = searchParams.get("id");
 	const validar = await Consultar(String(id));
+
 	if (!validar) {
-		return NextResponse.json( false );
+		return new NextResponse(JSON.stringify({ success: false }), {
+			status: 404, // ou outro código de status apropriado
+			headers: { "Content-Type": "application/json" },
+		});
 	}
 
-	return NextResponse.json(true);
+	return new NextResponse(JSON.stringify({ success: true }), {
+		headers: { "Content-Type": "application/json" },
+		status: 200,
+	});
 }
