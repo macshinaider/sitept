f625600c1ba3c9bfa096d4fb3fe72a5a9296c58b
Lucas Santana <lucasadd15@gmail.com>
2024-01-21 20:28:51 -0300

    alter cod

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Header: React.FC = () => {
-    return (
-        <header className="text-center bg-black text-white rounded p-">
-            <h1 className="text-4xl font-bold">Larean Priston Tale</h1>
-            <p className="text-xl mt-2">Servidor Totalmente Free To Play.</p>
-        </header>
-    );
+	return (
+		<header className="bg-black">
+			<div className="container mx-auto flex items-center justify-center h-full ">
+				<div className="text-center">
+					<h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-white">
+						Larean Priston Tale
+					</h1>
+					<p className="text-2xl md:text-3xl lg:text-4xl mt-4 text-gray-300">
+						Servidor Totalmente Free To Play.
+					</p>
+				</div>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
