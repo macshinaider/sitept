4ca81da9553901a24c18770e71e22e40c642fd49
Lucas Santana <lucasadd15@gmail.com>
2024-01-21 18:05:07 -0300

    igf imagem para site

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,43 +1,64 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 interface GithubUser {
-    name: string;
-    bio: string;
-    avatar_url: string;
-    html_url: string;
+	name: string;
+	bio: string;
+	avatar_url: string;
+	html_url: string;
 }
 
 const Footer: React.FC = () => {
-    const [githubUser, setGithubUser] = useState<GithubUser | null>(null);
+	const [githubUser, setGithubUser] = useState<GithubUser | null>(null);
 
-    useEffect(() => {
-        axios.get('https://api.github.com/users/macshinaider')
-            .then(response => {
-                setGithubUser(response.data);
-            })
-            .catch(error => {
-                console.error('Erro ao buscar dados do GitHub', error);
-            });
-    }, []);
+	useEffect(() => {
+		axios
+			.get("https://api.github.com/users/macshinaider")
+			.then((response) => {
+				setGithubUser(response.data);
+			})
+			.catch((error) => {
+				console.error("Erro ao buscar dados do GitHub", error);
+			});
+	}, []);
 
-    return (
-        <footer className="p-4 bg-black text-white rounded">
-            {githubUser ? (
-                <div className="flex items-center justify-center space-x-4">
-                    <img className="w-16 h-16 rounded-full" src={githubUser.avatar_url} alt={githubUser.name} />
-                    <div>
-                        <h2 className="text-xl font-bold">{githubUser.name}</h2>
-                        <p>{githubUser.bio}</p>
-                        <a className="text-blue-200 hover:underline" href={githubUser.html_url}>Ver no GitHub</a>
-                    </div>
-                </div>
-            ) : (
-                <p>Carregando dados do GitHub...</p>
-            )}
-        </footer>
-    );
+	return (
+		<footer className="p-4 bg-black text-white rounded">
+			{githubUser ? (
+				<div className="flex items-center justify-center space-x-4">
+					<img
+						className="w-16 h-16 rounded-full"
+						src={githubUser.avatar_url}
+						alt={githubUser.name}
+					/>
+					<div>
+						<h2 className="text-xl font-bold">{githubUser.name}</h2>
+						<p>{githubUser.bio}</p>
+						<a
+							className="text-blue-200 hover:underline"
+							href={githubUser.html_url}
+						>
+							Ver no GitHub
+						</a>
+					</div>
+				</div>
+			) : (
+				<p>Carregando dados do GitHub...</p>
+			)}
+
+			<a
+				href="https://www.xtremetop100.com/in.php?site=1132375350"
+				title="PristonTale Private Server"
+			>
+				<img
+					src="https://www.xtremeTop100.com/votenew.jpg"
+					border="0"
+					alt="PristonTale Private Server"
+				/>
+			</a>
+		</footer>
+	);
 };
 
 export default Footer;
