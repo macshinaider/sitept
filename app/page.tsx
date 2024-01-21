1456def47c9c76223b69b877b8eb041726dd0c91
Lucas Santana <lucasadd15@gmail.com>
2024-01-21 13:01:00 -0300

    alter codigo

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,19 @@ import RanquePVP from "@/components/rankpvp";
 import RanqueEXP from "@/components/rankxp";
 import Link from "next/link";
 import Head from "next/head";
-
+import { useState } from "react";
+import Modal from "react-modal";
 
 export default function Home() {
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+	function openModal() {
+		setModalIsOpen(true);
+	}
+
+	function closeModal() {
+		setModalIsOpen(false);
+	}
+	
 	function DiscordRedirect() {
 		window.open("https://discord.gg/A7RtTbavEW", "_blank");
 	}
@@ -32,8 +42,7 @@ export default function Home() {
 						/>
 						<link rel="canonical" href="https://www.pt.larean.com.br" />
 					</Head>
-				</>			
-				
+				</>
 
 				<h1 className="hidden">Larean Priston Tale</h1>
 				<div className="flex gap-4 md:flex-col">
@@ -52,6 +61,11 @@ export default function Home() {
 					<button onClick={DiscordRedirect}>
 						<p className="bg-[#5865F2] hover:bg-blue-800 p-2 rounded-lg text-white">
 							Discord
+						</p>
+					</button>
+					<button onClick={openModal}>
+						<p className="bg-[#f2a258] hover:bg-blue-800 p-2 rounded-lg text-white">
+							Infos
 						</p>
 					</button>
 					<button
@@ -80,92 +94,122 @@ export default function Home() {
 				</div>
 			</div>
 			<div className="flex justify-between gap-6 md:flex-col">
-			<iframe
-				src="https://discord.com/widget?id=1181632791935471636&theme=dark"
-				width="350"
-				height="500"
-				sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
-			></iframe>
-			{/* <div className="flex flex-col items-start bg-gray-100 p-4 rounded-lg shadow-lg">
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">Level Inicial:</a>
-					<a className="text-green-600">1</a>
-				</h1>
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">Level Maximo:</a>
-					<a className="text-green-600">130</a>
-				</h1>
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">XP rate:</a>
-					<a className="text-green-600">13x</a>
-				</h1>
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">Drop rate:</a>
-					<a className="text-red-600">Hard</a>
-				</h1>
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">Drops até:</a>
-					<a className="text-green-600">105</a>
-				</h1>
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">Mapas até:</a>
-					<a className="text-green-600">ET3</a>
-				</h1>
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">Aging até:</a>
-					<a className="text-green-600">+20</a>
-				</h1>
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">
-						Sistema de roleta de BOSS
-					</a>
-				</h1>
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">Alerta BOSS Global</a>
-				</h1>
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">PVP no "K"</a>
-				</h1>
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">Sistema Vip</a>
-				</h1>
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">
-						Tudo que tem no jogo é dropavel até coins
-					</a>
-				</h1>
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">
-						Arena PVP com BOSS para os 3 colocados
-					</a>
-				</h1>
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">PvP Equilibrado</a>
-				</h1>
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">
-						3 Coroas de Sod com Status
-					</a>
-				</h1>
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">Destaque de classe</a>
-				</h1>
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">Logado Premiado</a>
-				</h1>
-				<h1 className="flex items-center space-x-2">
-					<a className="font-bold text-gray-800">Loja de Tempo</a>
-				</h1>
-				<h1 className="flex items-center space-x-2 ">
-					<a className="font-bold text-gray-800">Evento de perguntas</a>
-				</h1>
-				<h1 className="flex items-center space-x-2 font-bold text-gray-800">
-					Suporte é pelo Discord
-				</h1>
-			</div> */}
-		</div>
+				<iframe
+					src="https://discord.com/widget?id=1181632791935471636&theme=dark"
+					width="350"
+					height="500"
+					sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+				></iframe>
+			</div>
 
 			<Prompt />
+			<div>
+				<audio controls autoPlay muted hidden>
+					<source src="/bemvindo.mp3" type="audio/mp3" />
+					Your browser does not support the audio element.
+				</audio>
+			</div>
+			<Modal
+				isOpen={modalIsOpen}
+				onRequestClose={closeModal}
+				contentLabel="Informações"
+				style={{
+					overlay: {
+						backgroundColor: "rgba(0, 0, 0, 0.75)",
+					},
+					content: {
+						top: "50%",
+						left: "50%",
+						right: "auto",
+						bottom: "auto",
+						marginRight: "-50%",
+						transform: "translate(-50%, -50%)",
+						backgroundColor: "#fff",
+						borderRadius: "4px",
+						padding: "20px",
+					},
+				}}
+			>
+				<div className="flex flex-col items-start bg-gray-100 p-4 rounded-lg shadow-lg">
+					<div className="flex flex-col items-start bg-gray-100 p-4 rounded-lg shadow-lg">
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">Level Inicial</a>
+							<a className="text-green-600">1</a>
+						</h1>
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">Level Maximo:</a>
+							<a className="text-green-600">130</a>
+						</h1>
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">XP rate:</a>
+							<a className="text-green-600">13x</a>
+						</h1>
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">Drop rate:</a>
+							<a className="text-red-600">Hard</a>
+						</h1>
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">Drops até:</a>
+							<a className="text-green-600">105</a>
+						</h1>
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">Mapas até:</a>
+							<a className="text-green-600">ET3</a>
+						</h1>
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">Aging até:</a>
+							<a className="text-green-600">+20</a>
+						</h1>
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">
+								Sistema de roleta de BOSS
+							</a>
+						</h1>
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">Alerta BOSS Global</a>
+						</h1>
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">PVP no "K"</a>
+						</h1>
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">Sistema Vip</a>
+						</h1>
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">
+								Tudo que tem no jogo é dropavel até coins
+							</a>
+						</h1>
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">
+								Arena PVP com BOSS para os 3 colocados
+							</a>
+						</h1>
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">PvP Equilibrado</a>
+						</h1>
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">
+								3 Coroas de Sod com Status
+							</a>
+						</h1>
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">Destaque de classe</a>
+						</h1>
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">Logado Premiado</a>
+						</h1>
+						<h1 className="flex items-center space-x-2">
+							<a className="font-bold text-gray-800">Loja de Tempo</a>
+						</h1>
+						<h1 className="flex items-center space-x-2 ">
+							<a className="font-bold text-gray-800">Evento de perguntas</a>
+						</h1>
+						<h1 className="flex items-center space-x-2 font-bold text-gray-800">
+							Suporte é pelo Discord
+						</h1>
+					</div>
+				</div>
+			</Modal>
 		</main>
 	);
 }
